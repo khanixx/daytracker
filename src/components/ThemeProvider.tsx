9d11a6e738d650c7a110affee45986acfed965ac
khanixx <kamalievabylaikhan@gmail.com>
2026-05-20 00:46:16 +0500

    upd

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -3,16 +3,18 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
-// Вместо импорта из next-themes используем стандартный тип для children
-export function ThemeProvider({ children, ...props }: { children: React.ReactNode; [key: string]: any }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ 
+  children, 
+  ...props 
+}: { 
+  children: React.ReactNode; 
+  [key: string]: any 
+}) {
   return (
-    <NextThemesProvider
-      attribute="class"       // добавляет класс "dark" на <html>
-      defaultTheme="system"   // уважает системные настройки
-      disableTransitionOnChange={false}
+    <NextThemesProvider 
+      attribute="class" 
+      defaultTheme="system" 
+      enableSystem
       {...props}
     >
       {children}
